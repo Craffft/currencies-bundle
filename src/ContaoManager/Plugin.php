7996ef3741000cd44d785d4c344bb9dc9b60baf3
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:09:48 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,7 +1,32 @@
 <?php
 
-namespace Craffft\CraffftCurrenciesBundle\ContaoManager;
+/*
+ * This file is part of the Currencies Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-class Plugin
+declare(strict_types=1);
+
+namespace Craffft\CurrenciesBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\CurrenciesBundle\CraffftCurrenciesBundle;
+
+class Plugin implements BundlePluginInterface
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftCurrenciesBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['currencies']),
+        ];
+    }
 }
